3e4794e697a9eda182bea52de765b1924b7988ec
Leila Forouzan <lforouzan@advanceproj.org>
2023-06-15 12:49:50 -0700

    Use of Force - Remove unused packages.

--- IndicatorScripts/CrimeJustice/crim_use_of_force_2023.R
+++ IndicatorScripts/CrimeJustice/crim_use_of_force_2023.R
@@ -2,7 +2,7 @@
 
 ## Set up ----------------------------------------------------------------
 #install packages if not already installed
-list.of.packages <- c("DBI", "tidyverse","RPostgreSQL", "tidycensus", "readxl", "sf", "janitor", "stringr", "rlist", "pipeR", "data.table", "usethis")
+list.of.packages <- c("DBI", "tidyverse","RPostgreSQL", "tidycensus", "readxl", "sf", "janitor", "stringr", "data.table", "usethis")
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages)
 
@@ -15,8 +15,6 @@ library(tidycensus)
 library(DBI)
 library(janitor)
 library(stringr)
-library(rlist) # working within list
-library(pipeR) # data grouping within list
 library(data.table) # %like% operator
 library(usethis)
 
