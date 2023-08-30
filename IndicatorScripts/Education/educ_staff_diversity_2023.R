4bb50f08240e50d537dd27a7c67434163fd2eaac
Alexandra Baker <ABaker@catalystcalifornia.org>
2023-08-30 12:11:25 -0700

    renamed files

--- IndicatorScripts/Education/educ_staff_diversity_2023.R
+++ IndicatorScripts/Education/educ_staff_diversity_2023.R
@@ -1,3 +1,5 @@
+### Teacher & Staff Diversity RC v5 ### 
+
 #install packages if not already installed
 list.of.packages <- c("readr","tidyr","dplyr","DBI","RPostgreSQL","tidycensus", "rvest", "tidyverse", "stringr", "usethis")
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
@@ -33,7 +35,7 @@ con <- connect_to_db("rda_shared_data")
 # ## Run function to prep and export rda_shared_data table 
 # source("W:/Project/RACE COUNTS/Functions/rdashared_functions.R")
 # df <- get_cde_data(filepath, fieldtype, table_schema, table_name, table_comment_source, table_source) # function to create and export rda_shared_table to postgres db
-####
+
 
 #######Get enrollment data for population values------
 enrollment <- st_read(con, query = "select * from education.cde_multigeo_enrollment_2018_19")
@@ -41,8 +43,8 @@ enrollment <- st_read(con, query = "select * from education.cde_multigeo_enrollm
 enrollment <- enrollment %>% mutate(districtcode = ifelse(!is.na(districtcode),paste0(enrollment$countycode,enrollment$districtcode), NA))
 
 enrollment_df <- enrollment %>% filter(aggregatelevel %in% c("C", "T", "D") & charter == "All" & 
-                             
-                             reportingcategory %in% c("TA", "RB", "RI", "RA", "RF", "RH", "RP", "RT", "RW")) %>%
+                                         
+                                         reportingcategory %in% c("TA", "RB", "RI", "RA", "RF", "RH", "RP", "RT", "RW")) %>%
   
   #select just fields we need
   select(aggregatelevel, cdscode, countyname, districtname, districtcode, reportingcategory, cumulativeenrollment) 
@@ -60,13 +62,11 @@ enrollment_df$reportingcategory <- gsub("RH", "latino", enrollment_df$reportingc
 enrollment_df$reportingcategory <- gsub("RP", "nh_pacisl", enrollment_df$reportingcategory)
 enrollment_df$reportingcategory <- gsub("RT", "nh_twoormor", enrollment_df$reportingcategory)
 enrollment_df$reportingcategory <- gsub("RW", "nh_white", enrollment_df$reportingcategory)
-enrollment_df <- rename(enrollment_df,c("geoname" = "countyname"))
 
 
 #pivot wider
-enrollment_wide <- enrollment_df %>% pivot_wider(names_from = reportingcategory, names_glue = "{reportingcategory}_pop", 
-                                     values_from = cumulativeenrollment)
-enrollment_wide$geoname[enrollment_wide$geoname =='State'] <- 'California'   # update state rows' geoname field values
+enrollment_wide <- enrollment_df %>% pivot_wider(names_from = reportingcategory, names_glue = "{reportingcategory}_pop", values_from = cumulativeenrollment)
+enrollment_wide$countyname[enrollment_wide$countyname =='State'] <- 'California'   # update state row's countyname field values
 
 
 #get county Geoids
@@ -84,10 +84,9 @@ names(ca) <- c("geoid", "geoname")
 
 
 #add county geoids
-enrollment_wide <- merge(x=ca,y=enrollment_wide,by="geoname", all=T)
+enrollment_wide <- left_join(x=enrollment_wide,y=ca,by=c("countyname"="geoname")) %>% mutate(geoid = ifelse(aggregatelevel=="D", NA, geoid))
 # add state geoid
-enrollment_wide <- within(enrollment_wide, geoid[geoname == 'California'] <- '06')
-enrollment_wide$geoname <- str_to_title(enrollment_wide$geoname)
+enrollment_wide <- within(enrollment_wide, geoid[countyname == 'California'] <- '06')
 
 ###### Get staff demographics data ----
 staff_demo <- st_read(con, query = "select * from education.cde_2018_19_staff_demo")
@@ -95,7 +94,7 @@ staff_demo$countycode <- stringr::str_extract(staff_demo$districtcode, "^.{2}")
 #create a countycode by extracting the first two string from the disctrictcode
 staff_demo$countyname <- str_to_title(staff_demo$countyname)
 staff_demo$countyname <- trimws(staff_demo$countyname) #remove spacing from string, you need to remove this space so you can join by geoname to add county geoids
-# # View(staff_demo)
+# View(staff_demo)
 
 # select only needed 
 df <- staff_demo %>% select(districtcode, countycode, countyname, districtname, ethnicgroup)
@@ -125,84 +124,67 @@ df_county$aggregatelevel <- "C"
 
 df_district_county <- rbind(df_district, df_county)
 #add county geoids
-df_district_county <- df_district_county %>% dplyr::rename("geoname"="countyname")
-df_district_county <- left_join(x=ca,y=df_district_county,by="geoname") 
+df_district_county <- left_join(x=df_district_county,y=ca,by=c("countyname"="geoname")) %>% mutate(geoid = ifelse(aggregatelevel=="D", NA, geoid))
 # View(df_district_county)
-#now add the state data -----
-df_state <- df_district %>% ungroup() %>% group_by(ethnicgroup) %>%  select(-c(countycode, districtcode,countyname,districtname,aggregatelevel)) %>% mutate(raw = n()) %>% unique()
+
+# add the state data -----
+df_state <- df_district %>% ungroup() %>% group_by(ethnicgroup) %>% select(-c(countycode,districtcode,countyname,districtname,aggregatelevel)) %>% mutate(raw = n()) %>% unique()
 df_state$districtcode <- NA
 df_state$districtname <- NA
 df_state$countycode <- NA
 df_state$countyname <- 'California'
 df_state$aggregatelevel <- 'T'
 df_state$geoid <- '06' # add state geoid
-df_state <- df_state %>% dplyr::rename("geoname"="countyname")
 # View(df_state)
 
 df_subset <- rbind(df_district_county, df_state)
 # View(df_subset)
 
-
-
 #pivot wider
-df_wide <- df_subset %>% 
-  pivot_wider(names_from = "ethnicgroup", values_from = "raw", names_glue = "{ethnicgroup}_raw") %>% select(-not_reported_raw)
-
-
-
+df_wide <- df_subset %>% pivot_wider(names_from = "ethnicgroup", values_from = "raw", names_glue = "{ethnicgroup}_raw") %>% select(-not_reported_raw)
 # View(df_wide)
 
 #join together to calculate rate----
-df_enroll_staff <- left_join(enrollment_wide, df_wide, by= c("districtcode", "geoname", "districtname", "geoid", "aggregatelevel"))
+df_enroll_staff <- right_join(enrollment_wide, df_wide, by= c("districtcode", "countyname", "districtname", "geoid", "aggregatelevel"))
 # View(df_enroll_staff)
 
 #screen data
-#set population screen threshhold
+#set population screen threshold
 pop_screen <- 100
-#pop screen on number of chronically absent students (raw) #at the state or county level the threshhold should probably be set to 20 but at the district level it doesn't male sense soI set it to zero
 df_enroll_staff <- df_enroll_staff %>% mutate(
-                                  total_raw = ifelse(total_pop < pop_screen, NA, total_raw),
-                                  nh_black_raw = ifelse(nh_black_pop < pop_screen, NA, nh_black_raw),
-                                  nh_aian_raw = ifelse(nh_aian_pop < pop_screen, NA, nh_aian_raw),
-                                  nh_asian_raw = ifelse(nh_asian_pop < pop_screen, NA, nh_asian_raw),
-                                  nh_filipino_raw = ifelse(nh_filipino_pop < pop_screen, NA, nh_filipino_raw),
-                                  latino_raw =   ifelse(latino_pop < pop_screen, NA, latino_raw),
-                                  nh_pacisl_raw =  ifelse(nh_pacisl_pop < pop_screen, NA, nh_pacisl_raw),
-                                  nh_twoormor_raw =  ifelse(nh_twoormor_pop < pop_screen, NA, nh_twoormor_raw),
-                                  nh_white_raw =  ifelse(nh_white_pop < pop_screen, NA, nh_white_raw),
-                                  
-                     total_rate = ifelse(is.na(total_raw) | is.na(total_pop) | total_pop == 0, NA, (total_raw / total_pop)*100),
-                     nh_black_rate = ifelse(is.na(nh_black_raw) | is.na(nh_black_pop) | nh_black_pop == 0, NA, (nh_black_raw / total_pop)*100),
-                     nh_aian_rate = ifelse(is.na(nh_aian_raw) | is.na(nh_aian_pop) |  nh_aian_pop == 0, NA, (nh_aian_raw / total_pop)*100),
-                     nh_asian_rate = ifelse(is.na(nh_asian_raw) | is.na(nh_asian_pop) | nh_asian_pop == 0, NA, (nh_asian_raw / total_pop)*100),
-                     nh_filipino_rate = ifelse(is.na(nh_filipino_raw) | is.na(nh_filipino_pop) | nh_filipino_pop == 0, NA, (nh_filipino_raw / total_pop)*100),
-                     latino_rate = ifelse(is.na(latino_raw) | is.na(latino_pop) | latino_pop == 0, NA, (latino_raw / total_pop)*100),
-                     nh_pacisl_rate = ifelse(is.na(nh_pacisl_raw) | is.na(nh_pacisl_pop) | nh_pacisl_pop == 0, NA, (nh_pacisl_raw / total_pop)*100),
-                     nh_white_rate = ifelse(is.na(nh_white_raw) | is.na(nh_white_pop) | nh_white_pop == 0, NA, (nh_white_raw / total_pop)*100),
-                     nh_twoormor_rate = ifelse(is.na(nh_twoormor_raw) | is.na(nh_twoormor_pop) | nh_twoormor_pop == 0, NA, (nh_twoormor_raw / total_pop)*100))
+  total_raw = ifelse(total_pop < pop_screen, NA, total_raw),
+  nh_black_raw = ifelse(nh_black_pop < pop_screen, NA, nh_black_raw),
+  nh_aian_raw = ifelse(nh_aian_pop < pop_screen, NA, nh_aian_raw),
+  nh_asian_raw = ifelse(nh_asian_pop < pop_screen, NA, nh_asian_raw),
+  nh_filipino_raw = ifelse(nh_filipino_pop < pop_screen, NA, nh_filipino_raw),
+  latino_raw =   ifelse(latino_pop < pop_screen, NA, latino_raw),
+  nh_pacisl_raw =  ifelse(nh_pacisl_pop < pop_screen, NA, nh_pacisl_raw),
+  nh_twoormor_raw =  ifelse(nh_twoormor_pop < pop_screen, NA, nh_twoormor_raw),
+  nh_white_raw =  ifelse(nh_white_pop < pop_screen, NA, nh_white_raw),
+  
+  total_rate = ifelse(is.na(total_raw) | is.na(total_pop) | total_pop == 0, NA, (total_raw / total_pop)*100),
+  nh_black_rate = ifelse(is.na(nh_black_raw) | is.na(nh_black_pop) | nh_black_pop == 0, NA, (nh_black_raw / total_pop)*100),
+  nh_aian_rate = ifelse(is.na(nh_aian_raw) | is.na(nh_aian_pop) |  nh_aian_pop == 0, NA, (nh_aian_raw / total_pop)*100),
+  nh_asian_rate = ifelse(is.na(nh_asian_raw) | is.na(nh_asian_pop) | nh_asian_pop == 0, NA, (nh_asian_raw / total_pop)*100),
+  nh_filipino_rate = ifelse(is.na(nh_filipino_raw) | is.na(nh_filipino_pop) | nh_filipino_pop == 0, NA, (nh_filipino_raw / total_pop)*100),
+  latino_rate = ifelse(is.na(latino_raw) | is.na(latino_pop) | latino_pop == 0, NA, (latino_raw / total_pop)*100),
+  nh_pacisl_rate = ifelse(is.na(nh_pacisl_raw) | is.na(nh_pacisl_pop) | nh_pacisl_pop == 0, NA, (nh_pacisl_raw / total_pop)*100),
+  nh_white_rate = ifelse(is.na(nh_white_raw) | is.na(nh_white_pop) | nh_white_pop == 0, NA, (nh_white_raw / total_pop)*100),
+  nh_twoormor_rate = ifelse(is.na(nh_twoormor_raw) | is.na(nh_twoormor_pop) | nh_twoormor_pop == 0, NA, (nh_twoormor_raw / total_pop)*100))
 # View(df_enroll_staff)
 
 
-####### GET COUNTY & SCHOOL DISTRICT GEOIDS ##### ---------------------------------------------------------------------
-census_api_key(census_key1, overwrite=TRUE) # In practice, may need to include install=TRUE if switching between census api keys
-Sys.getenv("CENSUS_API_KEY") # confirms value saved to .renviron
-
-# get school district geoids - pull in active district records w/ geoids from CDE schools' list (NCES District ID)
-districts <- st_read(con, query = "SELECT cdscode, ncesdist AS district_geoid, district FROM education.cde_public_schools_2021_22 WHERE ncesdist <> '' AND right(cdscode,7) = '0000000' AND statustype = 'Active'")
+####### GET SCHOOL DISTRICT GEOIDS ##### ---------------------------------------------------------------------
+# get school district geoids - pull in active district records w/ geoids from CDE schools' list (NCES District ID).
+## can't get archival data, so using 2019-20 bc that is closest match to data vintage (2018-19)
+districts <- st_read(con, query = "SELECT cdscode, ncesdist AS geoid FROM education.cde_public_schools_2019_20 WHERE ncesdist <> '' AND right(cdscode,7) = '0000000' AND statustype = 'Active'")
 # View(districts)
 
-district_match <- left_join(df_enroll_staff,districts,by=c('districtname' = 'district', "cdscode")) %>% ungroup() 
+# join dist geoids to data df, coalesce combined geoid col, drop separate county/state and district geoid cols
+df_final <- df_enroll_staff %>% left_join(districts, by="cdscode") %>% mutate(geoid = coalesce(geoid.x, geoid.y)) %>% 
+  select(-c(geoid.x, geoid.y, countycode)) %>% relocate(geoid, .before = everything())
 
-district_match <- district_match %>% mutate(geoid=ifelse(aggregatelevel=="D",district_geoid,geoid))
-# View(district_match)
-
-df_final <- district_match
-df_final <- df_final %>% relocate(geoid, cdscode) 
-df_final <- filter(df_final, !is.na(geoid)) # remove records without fips codes
-df_final <- df_final %>% unique() %>% select(-c(district_geoid))
-
-d <- df_final 
-
+d <- df_final %>% drop_na(geoid) # drop records without geoids, these are all districts
 
 ####################################################################################################################################################
 ############## CALC RACE COUNTS STATS ##############
@@ -224,8 +206,8 @@ state_table <- d[d$aggregatelevel == 'T', ]
 
 #calculate STATE z-scores
 state_table <- calc_state_z(state_table)
-state_table <- state_table %>% dplyr::rename("state_id" = "geoid", "state_name" = "geoname") %>% select(-c(districtname, districtcode, cdscode, aggregatelevel))
-# View(state_table)
+state_table <- state_table %>% dplyr::rename("state_id" = "geoid", "state_name" = "countyname") %>% select(-c(districtname, districtcode, cdscode, aggregatelevel))
+View(state_table)
 
 #remove state from county table
 county_table <- d[d$aggregatelevel == 'C', ]
@@ -233,19 +215,17 @@ county_table <- d[d$aggregatelevel == 'C', ]
 #calculate COUNTY z-scores
 county_table <- calc_z(county_table)
 county_table <- calc_ranks(county_table)
-county_table <- county_table %>% dplyr::rename("county_id" = "geoid", "county_name" = "geoname") %>% select(-c(districtname, districtcode, cdscode, aggregatelevel))
-# View(county_table)
+county_table <- county_table %>% dplyr::rename("county_id" = "geoid", "county_name" = "countyname") %>% select(-c(districtname, districtcode, cdscode, aggregatelevel))
+View(county_table)
 
 #remove county/state from place table -----
-city_table <- d[d$aggregatelevel == 'D', ]
+city_table <- d[d$aggregatelevel == 'D', ] %>% select(-c(aggregatelevel, districtcode))
 
 #calculate DISTRICT z-scores
 city_table <- calc_z(city_table)
 city_table <- calc_ranks(city_table)
-city_table <- city_table %>% 
-  dplyr::rename("dist_geoid" = "geoid", "district_name" = "districtname", "cds_code" = "cdscode") %>% 
-  select(-c(aggregatelevel, geoname, districtcode))
-# View(city_table)
+city_table <- city_table %>% dplyr::rename("dist_id" = "geoid", "district_name" = "districtname", "county_name" = "countyname") %>% relocate(county_name, .after = district_name)
+View(city_table)
 
 
 ###update info for postgres tables###
@@ -255,12 +235,8 @@ city_table_name <- "arei_educ_staff_diversity_district_2023"
 rc_schema <- "v5"
 
 indicator <- "Staff and Teacher Diversity Count and Rate. This data is"
-
 source <- "CDE 2018-2019 https://www.cde.ca.gov/ds/ad/filesabd.asp"
 
-# #send tables to postgres
+# send tables to postgres
 # to_postgres(county_table,state_table)
-
-
 # city_to_postgres()
-
