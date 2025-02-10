3e085733a24021a195f0a35e212f7e95490259d5
Leila Forouzan <lforouzan@advanceproj.org>
2025-02-10 12:16:05 -0800

    Updated Employment. Updated RC_Functions.R to allow Leg Dist calcs.

--- IndicatorScripts/Economic/econ_employment_2025.R
+++ IndicatorScripts/Economic/econ_employment_2025.R
@@ -1,4 +1,4 @@
-## Employment for RC v6 ##
+## Employment for RC v7 ##
 #install packages if not already installed
 list.of.packages <- c("readr","tidyr","dplyr","DBI","RPostgreSQL","tidycensus", "rvest", "tidyverse", "stringr", "usethis", "sf")
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
@@ -21,9 +21,9 @@ con <- connect_to_db("rda_shared_data")
 
 
 ############## UPDATE FOR SPECIFIC INDICATOR HERE ##############
-curr_yr = 2022      # you MUST UPDATE each year
-rc_yr = '2024'      # you MUST UPDATE each year
-rc_schema <- "v6"   # you MUST UPDATE each year
+curr_yr = 2023      # you MUST UPDATE each year
+rc_yr = '2025'      # you MUST UPDATE each year
+rc_schema <- "v7"   # you MUST UPDATE each year
 cv_threshold = 40         
 pop_threshold = 150       
 asbest = 'max'            
@@ -31,7 +31,9 @@ schema = 'economic'
 table_code = 's2301'
 
 
-df_wide_multigeo <- st_read(con, query = paste0("select * from ",schema,".acs_5yr_",table_code,"_multigeo_",curr_yr," WHERE geolevel IN ('place', 'county', 'state')")) # import rda_shared_data table
+df_wide_multigeo <- dbGetQuery(con, paste0("select * from ",schema,".acs_5yr_",table_code,"_multigeo_",curr_yr," WHERE geolevel IN ('place', 'county', 'state', 'sldu', 'sldl')")) # import rda_shared_data table
+df_wide_multigeo$geolevel <- gsub("sldu", "upper", df_wide_multigeo$geolevel)  # update geolevel for sldu
+df_wide_multigeo$geolevel <- gsub("sldl", "lower", df_wide_multigeo$geolevel)  # update geolevel for sldl
 
 ############## Pre-RC CALCS ##############
 source("https://raw.githubusercontent.com/catalystcalifornia/RaceCounts/main/Functions/rdashared_functions.R")
@@ -44,7 +46,8 @@ d <- df_screened
 ############## CALC RACE COUNTS STATS ##############
 
 #set source for RC Functions script
-source("https://raw.githubusercontent.com/catalystcalifornia/RaceCounts/main/Functions/RC_Functions.R")
+#source("https://raw.githubusercontent.com/catalystcalifornia/RaceCounts/main/Functions/RC_Functions.R")
+source("W:\\Project\\RACE COUNTS\\2025_v7\\RC_Github\\LF\\RaceCounts\\Functions\\RC_Functions.R")
 
 # Adds asbest value for RC Functions
 d$asbest = asbest
@@ -57,10 +60,13 @@ d <- calc_s_var(d)
 d <- calc_id(d)
 
 ### Split into geolevel tables
-#split into STATE, COUNTY, CITY tables 
+#split into STATE, COUNTY, CITY, SLDU, SLDL tables 
 state_table <- d[d$geolevel == 'state', ]
 county_table <- d[d$geolevel == 'county', ]
 city_table <- d[d$geolevel == 'place', ]
+upper_table <- d[d$geolevel == 'upper', ]
+lower_table <- d[d$geolevel == 'lower', ]
+
 
 #calculate STATE z-scores
 state_table <- calc_state_z(state_table) %>% dplyr::select(-c(geolevel))
@@ -80,25 +86,48 @@ city_table <- calc_z(city_table)
 city_table <- calc_ranks(city_table) %>% dplyr::select(-c(geolevel))
 View(city_table)
 
+#calculate SLDU z-scores
+upper_table <- calc_z(upper_table)
+
+## Calc SLDU ranks##
+upper_table <- calc_ranks(upper_table)
+View(upper_table)
+
+#calculate SLDL z-scores
+lower_table <- calc_z(lower_table)
+
+## Calc SLDL ranks##
+lower_table <- calc_ranks(lower_table)
+View(lower_table)
+
+## Bind sldu and sldl tables into one leg_table##
+leg_table <- rbind(upper_table, lower_table)
+
+
 #rename geoid to state_id, county_id, city_id
 colnames(state_table)[1:2] <- c("state_id", "state_name")
 colnames(county_table)[1:2] <- c("county_id", "county_name")
 colnames(city_table)[1:2] <- c("city_id", "city_name")
+colnames(leg_table)[1:2] <- c("leg_id", "leg_name")
 
 
-############### COUNTY, STATE, CITY METADATA  ##############
+############### COUNTY, STATE, CITY, SLDU, SLDL METADATA  ##############
 
 ### info for postgres tables will automatically update ###
 county_table_name <- paste0("arei_econ_employment_county_", rc_yr)      # See most recent RC Workflow SQL Views for table name (remember to update year)
 state_table_name <- paste0("arei_econ_employment_state_", rc_yr)        # See most recent RC Workflow SQL Views for table name (remember to update year)
 city_table_name <- paste0("arei_econ_employment_city_", rc_yr)          # See most recent RC Workflow SQL Views for table name (remember to update year)
+leg_table_name <- paste0("arei_econ_employment_leg_", rc_yr)            # See most recent RC Workflow SQL Views for table name (remember to update year)
+
+
 indicator <- paste0("Created on ", Sys.Date(), ". Employment to Population Rate (%)")                 # See most recent Indicator Methodology for indicator description
 start_yr <- curr_yr-4
-source <- paste0(start_yr,"-",curr_yr,"ACS 5-Year Estimates, Table S2301, https://data.census.gov/cedsci/")   # See most recent Indicator Methodology for source info
+source <- paste0(start_yr,"-",curr_yr," ACS 5-Year Estimates, Table S2301, https://data.census.gov/cedsci/")   # See most recent Indicator Methodology for source info
 
 
 ####### SEND TO POSTGRES #######
 #to_postgres(county_table, state_table)
 #city_to_postgres(city_table)
+#leg_to_postgres(leg_table)
 
 #dbDisconnect(con)
