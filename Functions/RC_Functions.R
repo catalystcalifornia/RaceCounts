4a830862e0cadf332b10d17d68fd517c10c7e988
Hillary Khan <HKhan@catalystcalifornia.org>
2025-02-21 07:02:57 -0800

    similar refactoring on other functions to calc_diff

--- Functions/RC_Functions.R
+++ Functions/RC_Functions.R
@@ -80,24 +80,25 @@ calc_best <- function(x) {
 
 #####calculate difference from best#####
 calc_diff <- function(x) {
-  # get geoid, raced rate and best columns
-  rates <- dplyr::select(x, geoid, geolevel, best, ends_with("_rate"), -starts_with("total_"), -ends_with("_no_rate"))  
-  # 2/21/23 added due to prior step resulting in dupes for overcrowding
+  # get geoid and geolevel, raced rate and best columns
+  rates <- dplyr::select(x, geoid, geolevel, best, ends_with("_rate"), 
+                         -starts_with("total_"), -ends_with("_no_rate"))  
+  
+  # Prep: added due to prior step resulting in dupes for overcrowding
   rates <- unique(rates) 
   
-  # pivot wide table to long on geoid & best cols
-  diff_long <- pivot_longer(rates, 4:ncol(rates), names_to="measure_rate", values_to="rate") %>%   
-    # calc diff from best
+  # Calculation: pivot to long on _rate cols to simplify diff calc 
+  # then pivot back to wide to join to original dataframe
+  diff <- rates %>%
+    pivot_longer(ends_with("_rate"), names_to="measure_rate", values_to="rate") %>%   
     mutate(diff=abs(best-rate)) %>%
-    #create new column names for diffs from best
-    mutate(measure_diff=sub("_rate", "_diff", measure_rate))      
-  
-  # pivot long table back to wide
-  diff_wide <- diff_long %>% dplyr::select(geoid, geolevel, measure_diff, diff) %>%     
+    mutate(measure_diff=sub("_rate", "_diff", measure_rate)) %>%
+    dplyr::select(geoid, geolevel, measure_diff, diff) %>%     
     pivot_wider(names_from=measure_diff, values_from=diff)
   
-  # join new diff from best columns back to original table
-  x <- x %>% left_join(diff_wide, by=c("geoid","geolevel"))                     
+  # join _diff columns back to original table
+  x <- x %>% 
+    left_join(diff, by=c("geoid","geolevel"))                     
   
   return(x)
 }
