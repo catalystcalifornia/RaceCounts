d3f47be97eb12d902725e543c10c5ad4002f3e47
Hillary Khan <HKhan@catalystcalifornia.org>
2025-02-20 16:02:40 -0800

    use consistent process - similar to count_values() (previous commit)

--- Functions/RC_Functions.R
+++ Functions/RC_Functions.R
@@ -46,19 +46,33 @@ count_values <- function(x) {
 
 #####calculate best rate#####
 calc_best <- function(x) {
-  options(scipen = 999) # disable scientific notation
-                    rates <- dplyr::select(x, asbest, ends_with("_rate"), -ends_with("_no_rate"), -total_rate)
-                    rates[rates==0] <- NA                                                         #sub-out zero rates for NA, so that min best calc works correctly
-                    if (min(rates$asbest) == 'max') {rates <- rates %>% rowwise() %>%
-                        dplyr::mutate(best = max(c_across(where(is.numeric)), na.rm = TRUE))
-                    } else
-                    if (min(rates$asbest) == 'min') {rates <- rates %>% rowwise() %>%
-                         dplyr::mutate(best = min(c_across(where(is.numeric)), na.rm = TRUE))
-                     }
-                    rates$best[rates$best == Inf | rates$best == -Inf] <- NA
-                    x$best <- rates$best
-
-return(x)
+  # disable scientific notation
+  options(scipen = 999) 
+  
+  rates <- x %>%
+    dplyr::select(geoid, geolevel, asbest, ends_with("_rate"), 
+                  -ends_with("_no_rate"), -total_rate) %>%
+    
+    # Prep: Replace 0 rates with NA 
+    # Excludes superficially low rates where asbest is "min"
+    mutate(across(ends_with("_rate"), ~if_else(. == 0, NA, .))) %>%
+    # Calculation
+    rowwise() %>%
+    mutate(best = case_when(
+      asbest == "max" ~ max(c_across(ends_with("_rate")), na.rm = TRUE),
+      asbest == "min" ~ min(c_across(ends_with("_rate")), na.rm = TRUE),
+      .default = NA
+    )) %>%
+    ungroup() %>%
+    # Clean: If all rates in a row are NA, max()/min() will return "Inf" values
+    # Replace Inf with NA 
+    mutate(best = if_else(is.infinite(best), NA, best)) %>%
+    dplyr::select(geoid, geolevel, best)
+  
+  x %>% 
+    left_join(rates, by = c("geoid", "geolevel"))
+  
+  return(x)
 }
 
 
