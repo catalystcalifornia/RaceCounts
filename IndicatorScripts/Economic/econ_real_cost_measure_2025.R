b1e23a6055922c9c6a6e45a2968ac5c34149655b
cringewald <cringewald@catalystcalifornia.org>
2025-07-28 15:37:26 -0700

    county and state add

--- IndicatorScripts/Economic/econ_real_cost_measure_2025.R
+++ IndicatorScripts/Economic/econ_real_cost_measure_2025.R
@@ -22,12 +22,12 @@ con2 <- connect_to_db("rda_shared_data")
 
 # define variables used in several places that must be updated each year
 curr_yr <- "2023"  # must keep same format
-data_yr <- "2021"
+data_yr <- "2023"
 rc_yr <- "2025"
 dwnld_url <- "https://unitedwaysca.org/download-the-public-data-set/"
 rc_schema <- "v7"
 
-qa_filepath <- ""
+qa_filepath <- "W:\\Project\\RACE COUNTS\\2025_v7\\Economic\\QA_Real_Cost_Measure.docx"
 
 # Read Data ---------------------------------------------------------------
 rcm <- dbGetQuery(con2, "SELECT * FROM economic.uw_2023_county_state_real_cost_measure")
@@ -73,12 +73,14 @@ rcm_subset <- rcm_subset %>% mutate(total_rate = (1 - pct_hh_below_rcm) * 100,
                                     nh_aian_rate = (1 - pct_nh_aian_hh_below_rcm) * 100,
                                     nh_other_rate = (1 - pct_nh_other_hh_below_rcm) * 100)
 
-# select just fields we want
-d <- rcm_subset %>% select(geoid, geoname, ends_with("_pop"), ends_with("_raw"), ends_with("_rate")) %>%
-  
-  #filter out regions
-  filter(!geoid %in% c("06117","06119","06121","06123","06125","06127","06129"))
+# add geolevel
+d <- rcm_subset %>% mutate(geolevel = ifelse(geoname == "California", "state", "county")) %>% 
 
+  # select just fields we want  
+  select(geoid, geoname, geolevel, ends_with("_pop"), ends_with("_raw"), ends_with("_rate")) %>%
+  
+  # filter out regions
+  filter(!geoid %in% c("06117","06119","06121","06123","06125","06127","06129"))
 
 
 # Screen data ----------------------------------------------------------
@@ -90,7 +92,8 @@ d <- rcm_subset %>% select(geoid, geoname, ends_with("_pop"), ends_with("_raw"),
 ############ geoid and total and raced _rate (following RC naming conventions) columns. If you use a rate calc function, you will need _pop and _raw columns as well.
 
 #set source for RC Functions script
-source("./Functions/RC_Functions.R")
+#source("./Functions/RC_Functions.R")
+source("W:/Project/RACE COUNTS/2025_v7/RC_Github/CR/Functions/RC_Functions.R")
 
 d$asbest = 'max'    #YOU MUST UPDATE THIS FIELD AS NECESSARY: assign 'min' or 'max'
 
@@ -122,44 +125,44 @@ View(county_table)
 
 
 #split LEG DIST into separate tables
-upper_table <- d[d$geolevel == 'sldu', ]
-lower_table <- d[d$geolevel == 'sldl', ]
+# upper_table <- d[d$geolevel == 'sldu', ]
+# lower_table <- d[d$geolevel == 'sldl', ]
+# 
+# #calculate SLDU z-scores and ranks
+# upper_table <- calc_z(upper_table)
+# 
+# upper_table <- calc_ranks(upper_table)
+# #View(upper_table)
+# 
+# #calculate SLDL z-scores and ranks
+# lower_table <- calc_z(lower_table)
+# 
+# lower_table <- calc_ranks(lower_table)
+# #View(lower_table)
+# 
+# ## Bind sldu and sldl tables into one leg_table##
+# leg_table <- rbind(upper_table, lower_table)
+# View(leg_table)
 
-#calculate SLDU z-scores and ranks
-upper_table <- calc_z(upper_table)
-
-upper_table <- calc_ranks(upper_table)
-#View(upper_table)
-
-#calculate SLDL z-scores and ranks
-lower_table <- calc_z(lower_table)
-
-lower_table <- calc_ranks(lower_table)
-#View(lower_table)
-
-## Bind sldu and sldl tables into one leg_table##
-leg_table <- rbind(upper_table, lower_table)
-View(leg_table)
-
-state_table <- state_table %>% dplyr::rename("state_name" = "geoname", "state_id" = "geoid")
-county_table <- county_table %>% dplyr::rename("county_name" = "geoname", "county_id" = "geoid")
-leg_table <- leg_table %>% dplyr::rename("leg_name" = "geoname", "leg_id" = "geoid")
+#state_table <- state_table %>% dplyr::rename("state_name" = "geoname", "state_id" = "geoid")
+#county_table <- county_table %>% dplyr::rename("county_name" = "geoname", "county_id" = "geoid")
+#leg_table <- leg_table %>% dplyr::rename("leg_name" = "geoname", "leg_id" = "geoid")
 
 
 
 ###update info for postgres tables will update automatically###
 county_table_name <- paste0("arei_econ_real_cost_measure_county_", rc_yr)
 state_table_name <- paste0("arei_econ_real_cost_measure_state_", rc_yr)
-leg_table_name <- paste0("arei_econ_real_cost_measure_leg_", rc_yr)
+#leg_table_name <- paste0("arei_econ_real_cost_measure_leg_", rc_yr)
 
 
 indicator <- paste0("Created on ", Sys.Date(), ". Households above the real cost of living (data from ", data_yr, "). This data is")
 source <- paste0("United Ways of California ", curr_yr, " ", dwnld_url, ". QA doc: ", qa_filepath)
 
-#to_postgres(county_table,state_table)
+to_postgres(county_table,state_table)
 #leg_to_postgres()
 
 
-dbDisconnect(con)
-dbDisconnect(con2)
+#dbDisconnect(con)
+#dbDisconnect(con2)
 
