c9be7169214b5d165f303ce82894635812984b3e
Leila Forouzan <lforouzan@advanceproj.org>
2025-09-03 12:45:42 -0700

    Commit QA'd changes to City Index scripts

--- IndexScripts/composite_index_city_calcs_2025.R
+++ IndexScripts/composite_index_city_calcs_2025.R
@@ -25,7 +25,7 @@ education_tables <- imap_dfr(education_tables_list, ~
 education_tables <- education_tables %>% mutate(indicator = gsub('arei_|api_', '', indicator)) %>% mutate(indicator =  gsub(paste0('_district_',rc_yr), '', indicator)) %>%
   rename(performance_z_score_unweighted = performance_z) %>% relocate(indicator, .after = disparity_z_score_unweighted)
 
-df_education_district <- education_tables %>% left_join(crosswalk, by = "dist_id") %>% filter(!is.na(city_id))
+df_education_district <- education_tables %>% left_join(crosswalk, by = "dist_id", relationship = 'many-to-many') %>% filter(!is.na(city_id))
 
 ## Aggregate District data to Cities: weighted averages by enrollment size per indicator (some cities have multiple districts but not every indicator will have multiple districts per city).
 education_tables_agg <- dist_data_to_city(df_education_district)
@@ -52,14 +52,14 @@ city_tables <- map2(city_tables, names(city_tables), ~ mutate(.x, indicator = .y
 city_tables_short <- lapply(city_tables, function(x) x %>% select(city_id, city_name, disparity_z, performance_z, indicator))
 
 city_tables_updated <-
-                      lapply(names(city_tables_short), function(i){
-                        x <- city_tables_short[[ i ]]
-                        # set new name for 2nd column
-                        names(x)[3] <- paste0(i, "_disp_z")
-                        names(x)[4] <- paste0(i, "_perf_z")
-                        # return
-                        x
-                      })
+  lapply(names(city_tables_short), function(i){
+    x <- city_tables_short[[ i ]]
+    # set new name for 2nd column
+    names(x)[3] <- paste0(i, "_disp_z")
+    names(x)[4] <- paste0(i, "_perf_z")
+    # return
+    x
+  })
 
 # only select columns we want
 city_tables_updated <- lapply(city_tables_updated, function(x) x %>% select(city_id, ends_with("disp_z"), ends_with("perf_z")))
@@ -67,14 +67,18 @@ city_tables_updated <- city_tables_updated %>% reduce(full_join) # convert list 
 names(city_tables_updated)[-1] <- gsub(x = names(city_tables_updated)[-1], pattern = paste0("_city_",rc_yr), replacement = "", names) # clean colnames
 
 # merge to get city names and education table
-city_tables_df <- city_tables_updated %>% arrange(city_id) %>% distinct(city_id, .keep_all = TRUE) %>% left_join(education_tables_agg) %>% left_join(arei_race_multigeo) %>% select(city_id, city_name, everything())
+city_tables_df <- city_tables_updated %>% arrange(city_id) %>%
+  distinct(city_id, .keep_all = TRUE) %>%
+  left_join(education_tables_agg) %>%
+  left_join(arei_race_multigeo) %>%
+  select(city_id, city_name, everything())
 
 # clean col names
 names(city_tables_df) <- sub('^arei_|^api_', '', names(city_tables_df))
 
 # remove cities that are actually universities/colleges
 # remove cities with no city_name
-city_tables_df <- city_tables_df %>% filter(!grepl('University', city_name)) %>% filter(!is.na(city_name)) %>% filter(!is.na(city_name))
+city_tables_df <- city_tables_df %>% filter(!grepl('University', city_name)) %>% filter(!is.na(city_name))
 
 # cap perf_z and disp_z values  at |3.5|. More info: https://catalystcalifornia.sharepoint.com/:w:/s/Portal/EX59kBOn8iRNrLuY1Sfk3JABT34dO3sj1j9fwkuUxLqUgQ?e=feyI80
 city_tables_capped <- clean_city_indicator_data_z(city_tables_df)
@@ -82,11 +86,11 @@ city_tables_capped <- clean_city_indicator_data_z(city_tables_df)
 # Count the number of valid disp_z and perf_z per city. We will use this later
 all_indicators_perf_count <- city_tables_df %>% select(city_id, ends_with("perf_z")) 
 all_indicators_perf_count <- all_indicators_perf_count %>% mutate(all_indicators_perf_count = rowSums(!is.na(all_indicators_perf_count[,c(-1)]))) %>%
-                                    select(city_id, all_indicators_perf_count)
+  select(city_id, all_indicators_perf_count)
 
 all_indicators_disp_count <- city_tables_df %>% select(city_id, ends_with("disp_z"))
 all_indicators_disp_count <- all_indicators_disp_count %>% mutate(all_indicators_disp_count = rowSums(!is.na(all_indicators_disp_count[,c(-1)]))) %>% 
-                                    select(city_id, all_indicators_disp_count)
+  select(city_id, all_indicators_disp_count)
 
 # Count number of indicators per issue area
 indicators <- names(city_tables) %>% as.data.frame()
@@ -94,7 +98,7 @@ educ_indicators <- unique(education_tables$indicator) %>% as.data.frame()
 print("Non-education indicators prepped and joined with Education indicators.")
 
 
-# Calc UNSCREENED Index ---------------------------------------------------
+# Calc Issue & Comp Indexes ---------------------------------------------------
 # Count number of indicators per issue
 crim_count <- length(grep("crim", indicators$.))
 demo_count <- length(grep("demo", indicators$.))
@@ -115,17 +119,8 @@ hous <- city_tables_capped %>% select(city_id, city_name, starts_with("hous")) %
 educ <- city_tables_capped %>% select(city_id, city_name, starts_with("educ")) %>% mutate(educ_count = educ_count) 
 
 
-## Set issue thresholds
-crim_threshold <- 1
-demo_threshold <- 1
-econ_threshold <- 2 # could be 3
-educ_threshold <- 2
-hlth_threshold <- 1
-hben_threshold <- 2
-hous_threshold <- 3
-
-
 # Calc issue z-scores (avg of issue's indicator z-scores)
+## thresholds set in the index script: composite_index_city_[year].R
 crim_index <- calculate_city_issue(crim, crim_count, crim_threshold) %>% rename_with(~ paste0('crime_and_justice', "_", .x), ends_with("z"))
 demo_index <- calculate_city_issue(demo, demo_count, demo_threshold) %>% rename_with(~ paste0('democracy', "_", .x), ends_with("z"))
 econ_index <- calculate_city_issue(econ, econ_count, econ_threshold) %>% rename_with(~ paste0('economic_opportunity', "_", .x), ends_with("z"))
@@ -157,22 +152,26 @@ all_index <- crim_index %>% select(city_id, ends_with("z")) %>% left_join(
 
 print("Issue indexes calculated and joined together.")
 
-# Calc composite index
-issue_area_threshold <- 4 # city must have at least 3 issue perf/disp z-scores or it will be suppressed
-indicator_threshold <- 13 # city must have data for at least 13 indicators or it will be suppressed
+# Apply pop screen depending on index_type selected by user. For SCREENED Index: Drop cities with pop below pop_threshold.
+all_index <- all_index %>% filter((index_type == "api_" & total_pop > 10000) | (index_type != "api_"))
 
-city_index <- calculate_city_index(all_index, issue_area_threshold, indicator_threshold)
+
+# Calc composite index
+# issue_area_threshold and indicator_threshold set in composite_index_city_[year].R
+city_index <- calculate_city_index(all_index, issue_area_threshold, indicator_threshold) %>%
+  filter(!is.na(disparity_z | performance_z))
 
 # add county, region data
-region_pop <- dbGetQuery(con, "SELECT  city_id, county_id, county_name, region FROM v5.arei_city_county_district_table")
-city_index <- city_index %>% left_join(region_pop, by = 'city_id')
-city_index <- unique(city_index) %>% select(city_id, city_name, all_indicators_disp_count, all_indicators_perf_count, issue_disp_count, issue_perf_count,
-                                    disparity_rank, performance_rank, quadrant, disparity_z, performance_z, crime_and_justice_disp_z, crime_and_justice_perf_z,
-                                    democracy_disp_z, democracy_perf_z, economic_opportunity_disp_z, economic_opportunity_perf_z, education_disp_z, education_perf_z,
-                                    healthy_built_environment_disp_z, healthy_built_environment_perf_z, health_care_access_disp_z, health_care_access_perf_z, 
-                                    housing_disp_z, housing_perf_z, region, county_id, county_name, total_pop, disparity_z_quartile, performance_z_quartile) %>% arrange(-disparity_z)
+region_pop <- dbGetQuery(con, paste0("SELECT  city_id, city_name, county_id, county_name, region FROM ", rc_schema, ".arei_city_county_district_table")) %>% unique()
+city_index <- city_index %>% select(-city_name) %>% left_join(region_pop, by = 'city_id')
+city_index <- unique(city_index) %>%
+  select(city_id, city_name, region, total_pop, all_indicators_disp_count, all_indicators_perf_count, issue_disp_count, issue_perf_count,
+         disparity_rank, performance_rank, quadrant, disparity_z, performance_z, crime_and_justice_disp_z, crime_and_justice_perf_z,
+         democracy_disp_z, democracy_perf_z, economic_opportunity_disp_z, economic_opportunity_perf_z, education_disp_z, education_perf_z,
+         healthy_built_environment_disp_z, healthy_built_environment_perf_z, health_care_access_disp_z, health_care_access_perf_z, 
+         housing_disp_z, housing_perf_z, county_id, county_name, disparity_z_quartile, performance_z_quartile) %>% arrange(-disparity_z)
 
-# clean city_name column
+# clean city_name column - just in case names are not clean already
 clean_geo_names <- function(x){
   
   x$city_name <- str_remove(x$city_name, ", California")
@@ -187,5 +186,3 @@ clean_geo_names <- function(x){
 city_index <- clean_geo_names(city_index)
 
 print("Composite index calculated.")
-
-
