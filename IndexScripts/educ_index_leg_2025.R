7f57de0ede2b7c5de49ef0401daa3cfe8f7fb360
Leila Forouzan <lforouzan@advanceproj.org>
2025-09-09 14:36:00 -0700

    Leg Indexes: Update join method

--- IndexScripts/educ_index_leg_2025.R
+++ IndexScripts/educ_index_leg_2025.R
@@ -84,12 +84,9 @@ c_7 <- clean_data_z(c_7, varname7)
 
 
 # Join Data Together ------------------------------------------------------
-c_index <- mutate(c_1,c_2)
-c_index <- mutate(c_index,c_3)
-c_index <- mutate(c_index,c_4)
-c_index <- mutate(c_index,c_5)
-c_index <- mutate(c_index,c_6)
-c_index <- mutate(c_index,c_7)
+index_list <- list(c_1, c_2, c_3, c_4, c_5, c_6, c_7)
+
+c_index <- index_list %>% reduce(full_join, by=c('leg_id', 'leg_name', 'geolevel'))
 
 colnames(c_index) <- gsub("performance", "perf", names(c_index))  # shorten col names
 colnames(c_index) <- gsub("disparity", "disp", names(c_index))    # shorten col names
