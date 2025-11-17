6722f8e84ddd516a55e74533e922dd85762c9920
Leila Forouzan <lforouzan@advanceproj.org>
2025-11-17 13:32:17 -0800

    Update Export_RCdata.R for Leg Dist

--- Functions/Export_RCdata.R
+++ Functions/Export_RCdata.R
@@ -1,5 +1,5 @@
-# Function to create raw data exports for county or state RC data
-## See example script where this fx is used: W:\Project\RACE COUNTS\2024_v6\RaceCounts\HPI_Ask\export_hpi_data.R
+# Function to create raw data exports for leg dist, county or state RC data
+## See example script where this fx is used: W:/Project/RACE COUNTS/2025_v7/RC_Github/LF/RaceCounts/Data/export_rc_data.R
 
 
 ## BEFORE RUNNING THE FUNCTION: ##############
@@ -8,54 +8,70 @@
 #### Eg: rc_list_query <- paste0("SELECT table_name FROM information_schema.tables WHERE table_type='BASE TABLE' AND table_schema='", curr_schema, "' AND table_name LIKE '%_", curr_yr, "';")
 ####     rc_list <- dbGetQuery(con, rc_list_query)
 
-### User must define geolevel 
-#### Eg: geolevel <- 'county'
+### User must define geotype 
+#### Eg: geotype <- 'county'  (or 'state' or 'leg')
 
 
-export_RCdata <- function(table_list, geolevel) {
+export_RCdata <- function(table_list, geotype) {
   
-  geolevel_list <- table_list %>%
-    filter(grepl(paste0("^arei_.*_", geolevel, "_", curr_yr, "$"), table_name)) %>%
+  geotype_list <- table_list %>%
+    filter(grepl(paste0("^arei_.*_", geotype, "_", curr_yr, "$"), table_name)) %>%
     arrange(table_name) %>% # alphabetize
     pull(table_name) # converts from df object to list; important for next steps using lapply
   
-  # import all tables on geolevel_list
-  geolevel_tables <- lapply(setNames(paste0("select * from ", curr_schema, ".", geolevel_list), geolevel_list), DBI::dbGetQuery, conn = con)
+  # import all tables on geotype_list
+  geotype_tables <- lapply(setNames(paste0("select * from ", curr_schema, ".", geotype_list), geotype_list), DBI::dbGetQuery, conn = con)
   
   # create column with indicator name
-  geolevel_tables <- map2(geolevel_tables, names(geolevel_tables), ~ mutate(.x, indicator = .y)) # create column with indicator name
+  geotype_tables <- map2(geotype_tables, names(geotype_tables), ~ mutate(.x, indicator = .y)) # create column with indicator name
+  
+  # create generic geoid col
+  existing_col <- paste0(geotype, "_id")
+  new_col <- 'geoid'
+  geotype_tables <- lapply(geotype_tables, function(x) x %>%
+                             mutate(!!sym(new_col) := !!sym(existing_col)))
+  
+  # Check if 'geolevel' column exists, and if not, create it with specific values
+  geotype_tables <- lapply(geotype_tables, function(x) {
+    # Check if 'geolevel' exists in the list element
+    if (!"geolevel" %in% names(x)) {
+      # If it doesn't exist, create it and assign NA values
+      x$geolevel <- geotype
+    }
+    return(x)
+  })
   
   # create a long df of rate values from every arei_ table
-  geolevel_tables_rate <- lapply(geolevel_tables, function(x) x %>% select(paste0(geolevel, "_id"), ends_with("_rate"), indicator))
+  geotype_tables_rate <- lapply(geotype_tables, function(x) x %>% select(ends_with("id"), geolevel, ends_with("_rate"), indicator))
   
-  geolevel_tables_rate <- imap_dfr(geolevel_tables_rate, ~
-                                     .x %>%
-                                     pivot_longer(cols = ends_with("_rate"),
-                                                  names_to = "race",
-                                                  values_to = "rate")) %>% 
+  geotype_tables_rate <- imap_dfr(geotype_tables_rate, ~ .x %>%
+                                    
+                                    pivot_longer(cols = ends_with("_rate"),
+                                                 names_to = "race",
+                                                 values_to = "rate")) %>% 
     mutate(race = (ifelse(race == 'rate', 'total', race)),
            race = gsub('_rate', '', race))
   
   
   # create a long df of race raw values from every arei_ table
-  geolevel_tables_raw <- lapply(geolevel_tables, function(x) x %>% select(paste0(geolevel, "_id"), ends_with("_raw"), indicator))
+  geotype_tables_raw <- lapply(geotype_tables, function(x) x %>% select(ends_with("id"), geolevel, ends_with("_raw"), indicator))
   
   # remove tables with no raws
-  geolevel_tables_raw <- geolevel_tables_raw[sapply(geolevel_tables_raw, function(x) "total_raw" %in% names(x))]
+  geotype_tables_raw <- geotype_tables_raw[sapply(geotype_tables_raw, function(x) "total_raw" %in% names(x))]
   
   #format
-  geolevel_tables_raw <- imap_dfr(geolevel_tables_raw, ~ .x %>%
-                                    pivot_longer(cols = ends_with("_raw"),
-                                                 names_to = "race",
-                                                 values_to = "raw")) %>% 
+  geotype_tables_raw <- imap_dfr(geotype_tables_raw, ~ .x %>%
+                                   pivot_longer(cols = ends_with("_raw"),
+                                                names_to = "race",
+                                                values_to = "raw")) %>% 
     mutate(race = (ifelse(race == 'raw', 'total', race)),
            race = gsub('_raw', '', race))
   
   
   # merge rate and raw long dfs
-  df_merged_geolevel <- geolevel_tables_rate %>% 
-    full_join(geolevel_tables_raw) %>%
-    rename(geoid = paste0(geolevel, "_id"))
+  df_merged_geotype <- geotype_tables_rate %>% 
+    left_join(geotype_tables_raw)
+  
   
   
   # format
@@ -71,5 +87,5 @@ export_RCdata <- function(table_list, geolevel) {
     rename(geo_name = name) %>%
     select(ends_with("_id"), ends_with("name"), geolevel, indicator, race, rate, raw)
   
-  return(df_geolevel)
+  return(df_geotype)
 }
