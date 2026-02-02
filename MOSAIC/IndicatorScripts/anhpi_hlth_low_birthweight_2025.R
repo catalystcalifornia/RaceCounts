4863519c40bfc39f586b80d4e873afb847427ffa
cringewald <cringewald@catalystcalifornia.org>
2026-02-02 14:21:42 -0800

    copy paste of asian with minor changes to make NHPI happen

--- MOSAIC/IndicatorScripts/anhpi_hlth_low_birthweight_2025.R
+++ MOSAIC/IndicatorScripts/anhpi_hlth_low_birthweight_2025.R
@@ -186,3 +186,60 @@ source <- paste0("US Department of Health and Human Services, Centers for Diseas
 
 #send tables to postgres
 # to_postgres(county_table,state_table,"mosaic")
+
+
+############## CALC NHPI RACE COUNTS STATS ##############
+############ To use the following RC Functions, 'd' will need the following columns at minimum: 
+############ geoid and total and raced _rate (following RC naming conventions) columns. If you use a rate calc function, you will need _pop and _raw columns as well.
+
+# remove NHPI columns leaving Asian columns
+d <- df_wide %>% select(geoid, geoname, geolevel, matches("Fiji|Marshall Islands|Micronesia|Palau|Samoa|Tonga")) %>%
+  
+  # calculate totals for RC stats
+  rowwise() %>% mutate(total_raw = sum(c_across(contains("_raw")), na.rm = TRUE)) %>% ungroup() %>%
+  rowwise() %>% mutate(total_births = sum(c_across(contains("_births")), na.rm = TRUE)) %>% ungroup() %>%
+  mutate(total_rate = total_raw/total_births*100)
+
+#set source for RC Functions script
+source(".\\Functions\\RC_Functions.R")
+
+d$asbest = 'min'    #YOU MUST UPDATE THIS FIELD AS NECESSARY: assign 'min' or 'max'
+
+d <- count_values(d) #calculate number of "_rate" values
+d <- calc_best(d) #calculate best rates -- be sure to update asbest accordingly before running this function.
+d <- calc_diff(d) #calculate difference from best
+d <- calc_avg_diff(d) #calculate (row wise) mean difference from best
+d <- calc_p_var(d) #calculate (row wise) population or sample variance. be sure to use calc_s_var for sample data or calc_p_var for population data.
+d <- calc_id(d) #calculate index of disparity
+
+
+#split STATE into separate table and format id, name columns
+state_table<- d[d$geoname == 'California', ]
+
+#calculate STATE z-scores
+state_table<- calc_state_z(state_table)
+
+state_table<- rename(state_table, state_id = geoid, state_name = geoname)
+View(state_table)
+
+#remove state from county table
+county_table<- d[d$geoname != 'California', ]
+
+#calculate COUNTY z-scores
+county_table<- calc_z(county_table)
+county_table<- calc_ranks(county_table)
+
+county_table <- rename(county_table, county_id = geoid, county_name = geoname)
+View(county_table)
+
+###info for postgres tables will auto-update based on variables at top of script###
+county_table_name <- paste0("nhpi_hlth_low_birthweight_county_",rc_yr)
+state_table_name <- paste0("nhpi_hlth_low_birthweight_state_",rc_yr)
+
+indicator <- paste0("Percentage of infants born at low birthweight (less than 2,500 grams or about 5lbs. 5oz) of all live births, by NHPI country of birth of mother. This data is")
+source <- paste0("US Department of Health and Human Services, Centers for Disease Control and Prevention (CDC), National Center for Health Statistics, Division of Vital Statistics, CDC WONDER Online Database (",curr_yr,"): https://wonder.cdc.gov/natality-expanded-current.html. Mothers birth country selected based on W:\\Data\\Health\\Births\\CDC\\Asian and NHPI countries.xlsx. QA doc: ", qa_filepath)
+
+#send tables to postgres
+# to_postgres(county_table,state_table,"mosaic")
+
+
