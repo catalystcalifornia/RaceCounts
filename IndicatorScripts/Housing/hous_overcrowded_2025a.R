9d33ffa951869192ca7bd692e967390aa83adda4
Leila Forouzan <lforouzan@advanceproj.org>
2026-05-05 09:55:52 -0700

    Edited Overcrowded

--- IndicatorScripts/Housing/hous_overcrowded_2025a.R
+++ IndicatorScripts/Housing/hous_overcrowded_2025a.R
@@ -1,8 +1,6 @@
-# B25014: Overcrowded Housing for RC v7
+# B25014: Overcrowded Housing for RC v7a - ACS 2017-21 for MOSAIC comparison ##
 #install packages if not already installed
-
-packages <- c("dplyr","data.table","tidycensus","sf","DBI","RPostgres","RPostgreSQL","stringr","tidyr","tigris","usethis", "here")  
-
+packages <- c("dplyr","data.table","tidycensus","sf","DBI","RPostgres","RPostgres","stringr","tidyr","tigris","usethis", "here")  
 
 install_packages <- packages[!(packages %in% installed.packages()[,"Package"])] 
 
@@ -24,11 +22,11 @@ options(scipen=999)
 source("W:\\RDA Team\\R\\credentials_source.R")
 con <- connect_to_db("rda_shared_data")
 
-qa_filepath <- "W:\\Project\\RACE COUNTS\\2025_v7\\Housing\\QA_Sheet_Overcrowded_Housing.docx"
+qa_filepath <- "Asana task: https://app.asana.com/1/110506578179264/project/1208663329421768/task/1214500131777384?focus=true"
 
 ### Update these variables each year ###
-curr_yr = 2023 # you MUST UPDATE each year
-rc_yr <- '2025'
+curr_yr = 2021 # you MUST UPDATE each year
+rc_yr <- '2025a'
 rc_schema <- 'v7'
 
 ### Variables that do not need updates ###
@@ -73,9 +71,9 @@ d <- calc_id(d) #calculate index of disparity
 #split into STATE, COUNTY, CITY, SLDU, SLDL tables 
 state_table <- d[d$geolevel == 'state', ]
 county_table <- d[d$geolevel == 'county', ]
-city_table <- d[d$geolevel == 'place', ]
-upper_table <- d[d$geolevel == 'sldu', ]
-lower_table <- d[d$geolevel == 'sldl', ]
+# city_table <- d[d$geolevel == 'place', ]
+# upper_table <- d[d$geolevel == 'sldu', ]
+# lower_table <- d[d$geolevel == 'sldl', ]
 
 #calculate STATE z-scores
 state_table <- calc_state_z(state_table) %>% select(-c(geolevel))
@@ -88,36 +86,36 @@ county_table <- calc_z(county_table)
 county_table <- calc_ranks(county_table) %>% select(-c(geolevel))
 View(county_table)
 
-#calculate CITY z-scores
-city_table <- calc_z(city_table)
-
-## Calc city ranks##
-city_table <- calc_ranks(city_table) %>% select(-c(geolevel))
-View(city_table)
-
-#calculate SLDU z-scores
-upper_table <- calc_z(upper_table)
-
-## Calc SLDU ranks##
-upper_table <- calc_ranks(upper_table)
-View(upper_table)
-
-#calculate SLDL z-scores
-lower_table <- calc_z(lower_table)
-
-## Calc SLDL ranks##
-lower_table <- calc_ranks(lower_table)
-View(lower_table)
-
-## Bind sldu and sldl tables into one leg_table##
-leg_table <- rbind(upper_table, lower_table)
-View(leg_table)
+# #calculate CITY z-scores
+# city_table <- calc_z(city_table)
+# 
+# ## Calc city ranks##
+# city_table <- calc_ranks(city_table) %>% select(-c(geolevel))
+# View(city_table)
+# 
+# #calculate SLDU z-scores
+# upper_table <- calc_z(upper_table)
+# 
+# ## Calc SLDU ranks##
+# upper_table <- calc_ranks(upper_table)
+# View(upper_table)
+# 
+# #calculate SLDL z-scores
+# lower_table <- calc_z(lower_table)
+# 
+# ## Calc SLDL ranks##
+# lower_table <- calc_ranks(lower_table)
+# View(lower_table)
+# 
+# ## Bind sldu and sldl tables into one leg_table##
+# leg_table <- rbind(upper_table, lower_table)
+# View(leg_table)
 
 #rename geoid to state_id, county_id, city_id, leg_id
 colnames(state_table)[1:2] <- c("state_id", "state_name")
 colnames(county_table)[1:2] <- c("county_id", "county_name")
-colnames(city_table)[1:2] <- c("city_id", "city_name")
-colnames(leg_table)[1:2] <- c("leg_id", "leg_name")
+# colnames(city_table)[1:2] <- c("city_id", "city_name")
+# colnames(leg_table)[1:2] <- c("leg_id", "leg_name")
 
 
 # ############## SEND COUNTY, STATE, CITY CALCULATIONS TO POSTGRES ##############
@@ -125,15 +123,15 @@ colnames(leg_table)[1:2] <- c("leg_id", "leg_name")
 ### info for postgres tables will auto update ###
 county_table_name <- paste0("arei_hous_overcrowded_county_", rc_yr)      # See most recent RC Workflow SQL Views for table name (remember to update year)
 state_table_name <- paste0("arei_hous_overcrowded_state_", rc_yr)        # See most recent RC Workflow SQL Views for table name (remember to update year)
-city_table_name <- paste0("arei_hous_overcrowded_city_", rc_yr)      # See most recent RC Workflow SQL Views for table name (remember to update year)
-leg_table_name <- paste0("arei_hous_overcrowded_leg_", rc_yr)          # See most recent RC Workflow SQL Views for table name (remember to update year)
+# city_table_name <- paste0("arei_hous_overcrowded_city_", rc_yr)      # See most recent RC Workflow SQL Views for table name (remember to update year)
+# leg_table_name <- paste0("arei_hous_overcrowded_leg_", rc_yr)          # See most recent RC Workflow SQL Views for table name (remember to update year)
 start_yr <- curr_yr -4
 
-indicator <- " Overcrowded Housing Units (%) (> 1 person per room)"                         # See most recent Indicator Methodology for indicator description
+indicator <- "MOSAIC comparision data. Overcrowded Housing Units (%) (> 1 person per room)"                         # See most recent Indicator Methodology for indicator description
 source <- paste0("ACS (", start_yr, "-", curr_yr,") 5-Year Estimates, Tables B25014B-I, https://data.census.gov/cedsci/") # See most recent Indicator Methodology for indicator description
 
 ####### SEND TO POSTGRES #######
-# to_postgres(county_table,state_table)
+to_postgres(county_table,state_table)
 # city_to_postgres(city_table)
 # leg_to_postgres(leg_table)
 # dbDisconnect(con)
