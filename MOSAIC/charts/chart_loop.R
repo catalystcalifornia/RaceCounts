b5d383a7951879d162f3c82aae78591e76ed6a04
Leila Forouzan <lforouzan@advanceproj.org>
2026-06-04 10:35:03 -0700

    Update MOSAIC Drilldown

--- MOSAIC/charts/chart_loop.R
+++ MOSAIC/charts/chart_loop.R
@@ -2,26 +2,37 @@
 library(rmarkdown)
 library(here)
 
-## Uncomment out Asian OR PacIsl charts, then run loop.
+## Uncomment out Asian OR PacIsl 'grp' and either 'bar' or 'drilld' indicators, then run loop.
 
 ###### ASIAN CHARTS ######
-# indicator_list <- c("voter_engagement", "overcrowded", "health_insurance", "officials")
 # grp <- "asian"
-
+# bar_indicators <- c("overcrowded", "officials")
+# drilld_indicators <- c("voter_engagement", "health_insurance") 
 
 
 ###### NHPI CHARTS ######
-# indicator_list <- c("connected_youth", "overcrowded", "health_insurance", "living_wage")
 # grp <- "nhpi"
+# bar_indicators <- c("health_insurance", "living_wage")
+# drilld_indicators <- c("connected_youth", "overcrowded")
 
 
+###### TALL BAR CHART LOOP ######
+for (r in drilld_indicators) {
+  render(
+    input = "./MOSAIC/charts/templates/drilldown.Rmd",
+    output_dir = paste0(getwd(),"/MOSAIC/charts"),
+    output_file = paste0(grp, "_", r, "_drilldown.html"),
+    params = list(drilld_indicators = r)
+  )
+}
 
-###### CHART LOOP ######
-for (r in indicator_list) {
+
+###### TALL BAR CHART LOOP ######
+for (r in bar_indicators) {
   render(
     input = "./MOSAIC/charts/templates/tall_bar.Rmd",
     output_dir = paste0(getwd(),"/MOSAIC/charts"),
     output_file = paste0(grp, "_", r, "_bar.html"),
-    params = list(indicator_list = r)
+    params = list(bar_indicators = r)
   )
 }
