d9e0bc53acf074d12f38f03bcf9f9cae5e343ee2
bakeralexan <abaker@catalystcalifornia.org>
2026-09-24 14:17:41 -0700

    some of the housing tables had a geolevel column thatw as creating empy na columns in the other tables when trying to full join so I just had to remove the geolevel column before creating the hous index then could rerun the composite index

--- IndexScripts/hous_index_2025.R
+++ IndexScripts/hous_index_2025.R
@@ -54,8 +54,8 @@ c_4 <- dbGetQuery(con, paste0("SELECT * FROM ", rc_schema, ".arei_hous_eviction_
 c_5 <- dbGetQuery(con, paste0("SELECT * FROM ", rc_schema, ".arei_hous_foreclosure_county_", rc_yr))
 c_6 <- dbGetQuery(con, paste0("SELECT * FROM ", rc_schema, ".arei_hous_homeownership_county_", rc_yr))
 c_7 <- dbGetQuery(con, paste0("SELECT * FROM ", rc_schema, ".arei_hous_overcrowded_county_", rc_yr))
-c_8 <- dbGetQuery(con, paste0("SELECT * FROM ", rc_schema, ".arei_hous_housing_quality_county_", rc_yr))
-c_9 <- dbGetQuery(con, paste0("SELECT * FROM ", rc_schema, ".arei_hous_student_homelessness_county_", rc_yr))
+c_8 <- dbGetQuery(con, paste0("SELECT * FROM ", rc_schema, ".arei_hous_housing_quality_county_", rc_yr)) %>% select(-geolevel)
+c_9 <- dbGetQuery(con, paste0("SELECT * FROM ", rc_schema, ".arei_hous_student_homelessness_county_", rc_yr))%>% select(-geolevel)
 c_10 <- dbGetQuery(con, paste0("SELECT * FROM ", rc_schema, ".arei_hous_subprime_county_", rc_yr))
 
 ## define variable names for clean_data_z function. you MUST UPDATE for each issue area. Copy from v6 index view.
@@ -143,7 +143,7 @@ View(index_table)
 index_table_name <- paste0("arei_hous_index_", rc_yr)
 index <- paste0("QA doc: ", qa_filepath, ". Includes all issue indicators. Issue area z-scores are the average z-scores for performance and disparity across all issue indicators. This data is")
 
-# index_to_postgres(index_table, rc_schema)
+index_to_postgres(index_table, rc_schema)
 dbDisconnect(con)
 
 
@@ -159,11 +159,11 @@ summary(comparison_c)
 disprk_report <- inner_join(index_table, county_v1, by = c("county_id","county_name"), suffix = c("_new", "_old")) %>%
   filter(housing_disparity_rank_new != housing_disparity_rank_old) %>%
   select(county_id, county_name, housing_disparity_rank_new, housing_disparity_rank_old)
-View(disprk_report)  # 171 counties moved ranks
+View(disprk_report)  # 46 counties moved ranks
 
 perfrk_report <- inner_join(index_table, county_v1, by = c("county_id","county_name"), suffix = c("_new", "_old")) %>%
   filter(housing_performance_rank_new != housing_performance_rank_old) %>%
   select(county_id, county_name, housing_performance_rank_new, housing_performance_rank_old)
-View(perfrk_report)  # 192 counties moved ranks so the new table should be kept over the old one
+View(perfrk_report)  # 0 counties moved ranks so the new table should be kept over the old one
 
 dbDisconnect(con_rc)
